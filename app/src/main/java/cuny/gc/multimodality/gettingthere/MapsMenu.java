3c5921e4f3a226b132e5dfd63cc41a93e307ee99
Steven Butler <srbutler@gmail.com>
2016-05-08 19:19:52 -0400

    Initial commit.

---
+++ app/src/main/java/cuny/gc/multimodality/gettingthere/MapsMenu.java
@@ -0,0 +1,13 @@
+package cuny.gc.multimodality.gettingthere;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MapsMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_menu);
+    }
+}
---
+++ app/src/main/java/cuny/gc/multimodality/gettingthere/NavMenu.java
@@ -0,0 +1,13 @@
+package cuny.gc.multimodality.gettingthere;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NavMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_menu);
+    }
+}
